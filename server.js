4862fe9bb250eb538c267114d01db5f0b927bc44
sofianeSghiouar <sofianesgh@gmail.com>
2022-05-27 00:03:50 +0200

    graphql crud finished with local data

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
 require("dotenv").config()
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
-const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLList, GraphQLInt, GraphQLNonNull } = require("graphql");
+const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLList, GraphQLInt, GraphQLNonNull, GraphQLBoolean } = require("graphql");
 const app = express();
 const port = process.env.port || 8000;
-const data = require("./data");
+const { authors, books } = require("./data");
 
 
 const AuthorType = new GraphQLObjectType({
     name: "Author",
-    description: 'This is author of a book',
+    description: 'This is the author of a book',
     fields: () => ({
         id: { type: GraphQLNonNull(GraphQLInt) },
         name: { type: GraphQLNonNull(GraphQLString) },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve: (author) => {
+                return books.filter(book => book.authorId === author.id)
+            }
+        }
     })
 })
 
 const BookType = new GraphQLObjectType({
     name: "Book",
-    description: 'This is infos of the book of an author',
+    description: 'This is the book infos of an author',
     fields: () => ({
         id: { type: GraphQLNonNull(GraphQLInt) },
         name: { type: GraphQLNonNull(GraphQLString) },
@@ -26,7 +32,7 @@ const BookType = new GraphQLObjectType({
         author: {
             type: AuthorType,
             resolve: (book) => {
-                return data.authors.find(author => author.id === book.authorId)
+                return authors.find(author => author.id === book.authorId)
             }
         }
     })
@@ -39,18 +45,143 @@ const rootQuery = new GraphQLObjectType({
         books: {
             type: new GraphQLList(BookType),
             description: "list of books",
-            resolve: () => data.books
+            resolve: () => books
         },
-        author: {
+        book: {
+            type: BookType,
+            description: "single book",
+            args: {
+                id: { type: GraphQLInt }
+            },
+            resolve: (parent, args) => {
+                return books.find(book => book.id === args.id)
+            }
+        },
+        authors: {
             type: new GraphQLList(AuthorType),
             description: "list of authors",
-            resolve: () => data.authors
+            resolve: () => authors
+        },
+        author: {
+            type: AuthorType,
+            description: "single author",
+            args: {
+                id: { type: GraphQLInt }
+            },
+            resolve: (parent, args) => {
+                return authors.find(author => author.id === args.id)
+            }
+        },
+    })
+})
+
+const rootMutation = new GraphQLObjectType({
+    name: "Mutation",
+    description: "root mutation",
+    fields: () => ({
+        addBook: {
+            type: BookType,
+            description: "add a book",
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString) },
+                authorId: { type: GraphQLNonNull(GraphQLInt) }
+            },
+            resolve: (parent, args) => {
+                const book = { id: books.length + 1, name: args.name, authorId: args.authorId }
+                books.push(book)
+                return book
+            }
+        },
+        addAuthor: {
+            type: AuthorType,
+            description: "add an author",
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString) },
+            },
+            resolve: (parent, args) => {
+                const author = { id: authors.length + 1, name: args.name }
+                authors.push(author)
+                return author
+            }
+        },
+        updateBook: {
+            type: BookType,
+            description: "update a book",
+            args: {
+                name: {
+                    type: GraphQLNonNull(GraphQLString)
+                },
+                id: { type: GraphQLInt }
+            },
+            resolve: (parent, args) => {
+                const book = books.find(book => book.id === args.id);
+                book.name = args.name;
+                return book
+            }
+        },
+        updateAuthor: {
+            type: AuthorType,
+            description: "update an author",
+            args: {
+                name: {
+                    type: GraphQLNonNull(GraphQLString)
+                },
+                id: { type: GraphQLInt }
+            },
+            resolve: (parent, args) => {
+                const author = authors.find(author => author.id === args.id);
+                author.name = args.name;
+                return author;
+            }
+        },
+        deleteBook: {
+            type: GraphQLBoolean,
+            description: "delete a book",
+            args: {
+                id: {
+                    type: GraphQLInt
+                }
+            },
+            resolve: async (parent, args) => {
+                const book = books.find(book => book.id === args.id)
+                const bookIndex = books.indexOf(book)
+                if (bookIndex === -1) {
+                    throw new Error("Book with this id has not been found")
+                }
+                try {
+                    books.splice(bookIndex, 1)
+                    return true
+                } catch (error) {
+                    console.log({ message: error.message });
+                }
+            }
+        },
+        deleteAuthor: {
+            type: GraphQLBoolean,
+            description: "delete an author",
+            args: {
+                id: { type: GraphQLInt }
+            },
+            resolve: async (parent, args) => {
+                const authorIndex = authors.indexOf(authors.find(author => author.id === args.id))
+
+                if (authorIndex === -1) {
+                    throw new Error("Author with this id has not been found")
+                }
+                try {
+                    authors.splice(authorIndex, 1)
+                    return true
+                } catch (error) {
+                    console.log({ message: error.message });
+                }
+            }
         }
     })
 })
 
 const schema = new GraphQLSchema({
-    query: rootQuery
+    query: rootQuery,
+    mutation: rootMutation
 })
 
 app.use("/graphql", graphqlHTTP({
